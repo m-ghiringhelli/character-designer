63795c196516c39f1750a52aee1f688b366f3ded
m-ghiringhelli <mghiring@gmail.com>
2022-01-26 11:45:22 -0800

    refactored makeStatsString to still pass

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 export function makeStatsString(headCount, middleCount, bottomCount) {
-    return `You clicked on the head 4 times, on the middle 5 times, and on the bottom 1 time. And everybody loves your character's catchphrases:`;
+    return `You clicked on the head ${headCount} times, on the middle ${middleCount} times, and on the bottom ${bottomCount} time. And everybody loves your character's catchphrases:`;
 }
