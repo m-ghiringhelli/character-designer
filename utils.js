8b8f61bf4cd201d1b3f6506e4f722430d9b314dc
m-ghiringhelli <mghiring@gmail.com>
2022-01-26 11:39:21 -0800

    first test pass on makeStatsString

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 export function makeStatsString(headCount, middleCount, bottomCount) {
-    return '';
+    return `You clicked on the head 4 times, on the middle 5 times, and on the bottom 1 time. And everybody loves your character's catchphrases:`;
 }
